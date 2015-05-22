c916e2d147e0394db7d91a15f1bb9b82e00a5689
Jordyn <jordyntacoronte@gmail.com>
2015-05-22 11:26:30 -0700

    finished second single view page

--- wp-content/themes/saveachristmas/single-pledges.php
+++ wp-content/themes/saveachristmas/single-pledges.php
@@ -5,7 +5,7 @@
     <strong>Email:</strong><?php the_content(); ?>
 
     <?php  $pledge_amount = get_post_custom_values('annual_donation_pledge_amount');
-            $donation_id = get_post_custom_values('annual_donation_campaign_id');
+            $donation_id = get_post_custom_values('annual-donation-campaign-id');
     ?>
     <strong>Pledge Amount:</strong> $<?php echo $pledge_amount[0];?>
     <strong>Campaign ID:</strong><?php echo $donation_id[0]; ?>
